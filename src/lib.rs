d12d881429d60188f249717410cd9a7d102b31ba
Dan <danmmwx@gmail.com>
2023-06-10 19:24:32 +0300

    Load templates and write email logic

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod routes;
 pub mod settings;
 pub mod startup;
 pub mod telemetry;
+
+pub static ENV: once_cell::sync::Lazy<minijinja::Environment<'static>> =
+    once_cell::sync::Lazy::new(|| {
+        let mut env = minijinja::Environment::new();
+        env.set_source(minijinja::Source::from_path("templates"));
+        env
+    });
