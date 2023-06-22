2ee779a946ca7d3c1f5d567b1ca79bb5f89e5593
Dan <danmmwx@gmail.com>
2023-06-22 21:35:13 +0300

    Activate confirmed user

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -1,5 +1,10 @@
+use actix_web::web::service;
+
 mod register;
+mod confirm_registration;
 
 pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
-    cfg.service(actix_web::web::scope("/users").service(register::register_user));
+    cfg.service(actix_web::web::scope("/users")
+    .service(register::register_user))
+    .service(confirm_registration::activate_new_user)
 }
