cf582a73b315cbbd81e1aaf22985b281da494c6a
Dan <danmmwx@gmail.com>
2023-06-23 06:06:12 +0300

    Login and user session

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -2,9 +2,11 @@ use actix_web::web::service;
 
 mod register;
 mod confirm_registration;
+mod login;
 
 pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(actix_web::web::scope("/users")
     .service(register::register_user))
     .service(confirm_registration::activate_new_user)
+    .service(login::get_user_who_is_active)
 }
