7df07c69b51210e3ae04fa5652db265d0ef86f2e
Dan <danmmwx@gmail.com>
2023-06-23 06:18:31 +0300

    Logging user out

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -3,10 +3,12 @@ use actix_web::web::service;
 mod register;
 mod confirm_registration;
 mod login;
+mod logout;
 
 pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(actix_web::web::scope("/users")
     .service(register::register_user))
-    .service(confirm_registration::activate_new_user)
-    .service(login::get_user_who_is_active)
+    .service(confirm_registration::confirm)
+    .service(login::login_user)
+    .service(logout::log_out)
 }
