20bbd81db219e68df0d6a934cb5ea455b72e8e09
Dan <danmmwx@gmail.com>
2023-07-12 02:56:32 +0300

    Install and setup TailwindCSS for SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="w-full h-[100vh] flex justify-center items-center">
+    <h1 class="text-3xl font-bold ">
+        Dieu est Grand!
+    </h1>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+      color: theme(colors.black)
+    }
+</style>
